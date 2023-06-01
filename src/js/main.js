d163f2e2af8c05d8f6a295ad9e2f2f649782f6cc
Faith Mansilungan <faith0094@stud.kea.dk>
2023-06-01 11:45:24 +0200

    Form rettelse

--- src/js/main.js
+++ src/js/main.js
@@ -33,3 +33,14 @@ afvisBtn.addEventListener("click", () => {
 accepterBtn.addEventListener("click", () => {
   cookieBox.style.display = "none";
 });
+
+// Active menu
+navbar = document.querySelector(".navbar").querySelectorAll("a");
+console.log(navbar);
+
+navbar.forEach((element) => {
+  element.addEventListener("click", function () {
+    navbar.forEach((nav) => nav.classList.remove("active"));
+    this.classList.add("active");
+  });
+});
